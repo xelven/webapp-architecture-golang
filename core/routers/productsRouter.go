b8c57c1c931e5cd5e4633ba494f6b2a67e4e65c1
wd-Allen <xelven@gmail.com>
2024-01-24 09:44:56 +0800

    add content type middleware for product router

--- core/routers/productsRouter.go
+++ core/routers/productsRouter.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"webapp-core/core/domain/product"
+	"webapp-core/core/routers/middleware"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog"
@@ -10,6 +11,8 @@ import (
 
 func NewProductsRouter(log *zerolog.Logger, db *gorm.DB) chi.Router {
 	products := chi.NewRouter()
+
+	products.Use(middleware.ContentTypeJSON)
 
 	productAPI := product.New(log, db)
 
